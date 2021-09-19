f9c57ab8c6f5916bcd19b7dc5e11d7e5f333e463
Kevin Perez <kevinp@cronapis.com>
2021-09-19 15:29:40 -0400

    init

---
+++ private-configuration-server/src/main/java/com/kurodevs/app/privateconfigurationserver/PrivateConfigurationServerApplication.java
@@ -0,0 +1,13 @@
+package com.kurodevs.app.privateconfigurationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateConfigurationServerApplication.class, args);
+	}
+
+}
---
+++ private-configuration-server/src/test/java/com/kurodevs/app/privateconfigurationserver/PrivateConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurodevs.app.privateconfigurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
